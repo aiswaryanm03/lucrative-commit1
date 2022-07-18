5eb2985f95a18836d7a715afb055645ce730f609
pande <pande@LAPTOP-7QPC96OE>
2022-07-18 10:28:59 +0530

    Changes done by Ayush

--- src/test/java/production/AishwaryaTest.java
+++ src/test/java/production/AishwaryaTest.java
@@ -7,11 +7,13 @@ public class AishwaryaTest {
 	public void Run()
 	{
 		System.out.println("i aded a run class in production pakage ");
+		System.out.println("Ayush done Changes");
 	}
 	@Test
 	public void RunTwo()
 	{
 		System.out.println("second run method");
+		System.out.println("Ayush done changes");
 	}
 
 }
--- src/test/java/production/OrganizationModuleTest.java
+++ src/test/java/production/OrganizationModuleTest.java
@@ -6,15 +6,18 @@ public class OrganizationModuleTest {
 	@Test
 	public void createOrgTest() {
 		System.out.println("createOrgTest executed");
+		System.out.println("Created by Ayush");
 	}
 	@Test
 	public void createOrganizationAndDeleteTest() {
 		System.out.println("createOrganizationAndDeleteTest executed");
+		System.out.println("Executed by Ayush");
 	}
 	@Test
 	public void changeorganizationinaishwarya()
 	{
 		System.out.println(" aishwarya organization");
+		System.out.println("Done Changes");
 	}
 		}
 
