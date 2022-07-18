8eb196554a9dd8672ce7314b4e23c709b8f8385b
iamsachin <iamsachin@DESKTOP-8RI6OF2>
2022-07-18 11:20:42 +0530

    removed some methods

--- src/test/java/production/AishwaryaTest.java
+++ src/test/java/production/AishwaryaTest.java
@@ -9,47 +9,7 @@ public class AishwaryaTest {
 		System.out.println("i aded a run class in production pakage ");
 		System.out.println("Ayush done Changes..");
 		System.out.println("Again done changes");
-	}
-	@Test
-	public void RunTwo()
-	{
-
-		System.out.println("second run method..");
-
-		System.out.println("second run method");
-
-		System.out.println("Ayush done changes.");
-		System.out.println("Third changes");
-		System.out.println("again done conflict changes");
-
-	}
-	@Test
-	public void RunTwo2()
-	{
-		System.out.println("second run method");
-		System.out.println("Third changes");
-		System.out.println("again done chamges");
-	}
-	@Test
-	public void RunThree3()
-	{
-
-		System.out.println("run three method.....");
-
+		System.out.println("removed some methods..");
 	}
 	
-	@Test
-	public void ConflictCheck()
-	{
-		System.out.println("check for the conflicts");
-
-
-		System.out.println("run three method");
-		System.out.println("again done changes");
-
-		System.out.println("run three method..");
-//>>>>>>> branch 'master' of https://github.com/aiswaryanm03/lucrative-commit1.git
-
-	}
-
 }
