7284bcba812e3da4daf7fe0408fc150d27e343d9
pande <pande@LAPTOP-7QPC96OE>
2022-07-19 11:33:59 +0530

    lala

--- src/test/java/production/AishwaryaTest.java
+++ src/test/java/production/AishwaryaTest.java
@@ -12,7 +12,7 @@ public class AishwaryaTest {
 		System.out.println("i aded a run class in production pakage........ ");
 		System.out.println("hi");
 		System.out.println("hello..");
-		System.out.println("test runned by ayush");
+		System.out.println("test runned by ayush ");
 		
 		
 	}
